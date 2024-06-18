8b13d0a235d1122ddf5d5fdf9e74dd2f79616709
Raunaksingh02 <158809081+Raunaksingh02@users.noreply.github.com>
2024-06-18 09:23:36 -0700

    ewf

--- src/Owner.js
+++ src/Owner.js
@@ -7,7 +7,7 @@ const socket = io("https://backendcafe-ceaj.onrender.com");
 
 function Owner(props) {
   const [requests, setRequests] = useState([]);
-  const audioRef = useRef(null);
+  const audioRef = useRef(new Audio('/alertsound.mp3'));
 
   useEffect(() => {
     socket.on('Request', (data) => {
@@ -27,38 +27,33 @@ function Owner(props) {
 
   const playAlertSound = () => {
     if (audioRef.current) {
-      audioRef.current.pause();
+      audioRef.current.currentTime = 0; // Ensure the sound plays from the start
+      audioRef.current.play();
     }
-    audioRef.current = new Audio('/alertsound.mp3');
-    audioRef.current.play();
   };
 
   const removeRequest = (index) => {
     if (audioRef.current) {
       audioRef.current.pause();
-      audioRef.current = null;
     }
     setRequests(requests.filter((_, i) => i !== index));
   };
 
   return (
     <div>
-      <div >
-        <div>
-
-          {requests.map((request, index) => (
-            <div key={index} className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
-              <strong className="font-bold">Incoming Call from {request.table}!</strong>
-              <span className="block sm:inline"> {request.query} at {request.time}</span>
-              <button
-                onClick={() => removeRequest(index)}
-                className="absolute top-0 bottom-0 right-0 px-4 py-3"
-              >
-                <span className="text-2xl">&times;</span>
-              </button>
-            </div>
-          ))}
-        </div>
+      <div>
+        {requests.map((request, index) => (
+          <div key={index} className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+            <strong className="font-bold">Incoming Call from {request.table}!</strong>
+            <span className="block sm:inline"> {request.query} at {request.time}</span>
+            <button
+              onClick={() => removeRequest(index)}
+              className="absolute top-0 bottom-0 right-0 px-4 py-3"
+            >
+              <span className="text-2xl">&times;</span>
+            </button>
+          </div>
+        ))}
       </div>
       <div>
         <div className='flex items-center'>
