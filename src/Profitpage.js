bdf88534bfe73d27307e000d90d566e4c5294be9
Raunaksingh02 <158809081+Raunaksingh02@users.noreply.github.com>
2024-06-20 03:46:55 -0700

    add the profit

--- src/Profitpage.js
+++ src/Profitpage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import backarrowlogo from "./images/backarrowlogo.png"
+import { Link } from 'react-router-dom';
 import './App.css';
 
 export default function Profitpage() {
@@ -41,22 +43,28 @@ export default function Profitpage() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <header className="bg-blue-600 text-white p-4">
-        <h1 className="text-2xl font-bold">Profit Page</h1>
-      </header>
+      <header className= " flex items-center bg-gray-400 text-white p-4">
+     <Link to="/Owner">
+      <img
+      src={backarrowlogo}
+      className='h-10 w-10'
+      />
+      </Link>
+      <h1 className="text-2xl font-bold ml-10">Profit Analysis</h1>
+    </header>
       <main className="flex-grow p-4">
         <div className="bg-white shadow-md rounded-lg p-6 mb-4">
-          <h2 className="text-xl font-semibold text-gray-800 mb-2">Select Date for Daily Profit</h2>
+          <h2 className="text-xl font-semibold text-gray-800 mb-2 text-center">Select Date for Daily Profit</h2>
           <DatePicker
             selected={selectedDate}
             onChange={(date) => setSelectedDate(date)}
             dateFormat="yyyy/MM/dd"
             className="block w-full mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
           />
-          <p className="text-lg text-gray-600 mt-4">Daily Profit: ${dailyProfit !== null ? dailyProfit.toFixed(2) : 'Loading...'}</p>
+          <p className="text-lg text-gray-600 mt-4 font-bold">Daily Profit: {dailyProfit !== null ? dailyProfit.toFixed(2) : 'Loading...'}</p>
         </div>
         <div className="bg-white shadow-md rounded-lg p-6">
-          <h2 className="text-xl font-semibold text-gray-800 mb-2">Select Month for Monthly Profit</h2>
+          <h2 className="text-xl font-semibold text-gray-800 mb-2 text-center">Select Month for Monthly Profit</h2>
           <DatePicker
             selected={selectedMonth}
             onChange={(date) => setSelectedMonth(date)}
@@ -64,7 +72,7 @@ export default function Profitpage() {
             showMonthYearPicker
             className="block w-full mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
           />
-          <p className="text-lg text-gray-600 mt-4">Monthly Profit: ${monthlyProfit !== null ? monthlyProfit.toFixed(2) : 'Loading...'}</p>
+          <p className="text-lg text-gray-600 mt-4 font-bold">Monthly Profit: {monthlyProfit !== null ? monthlyProfit.toFixed(2) : 'Loading...'}</p>
         </div>
       </main>
       <footer className="bg-gray-800 text-white p-4 text-center">
