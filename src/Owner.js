2c62f4d59493c3619ba14d41681a49e161c48e0d
Raunaksingh02 <158809081+Raunaksingh02@users.noreply.github.com>
2024-08-12 13:40:09 +0530

    add the following code

--- src/Owner.js
+++ src/Owner.js
@@ -19,6 +19,7 @@ import Heartbeat from "./Heartbeat";
 function Owner() {
   const [stats, setStats] = useState({});
   const [showModal, setShowModal] = useState(false);
+  const [showStats, setShowStats] = useState(true);
   const { logout } = useContext(AuthContext);
   const navigate = useNavigate();
   const [currentDate, setCurrentDate] = useState('');
@@ -61,7 +62,7 @@ function Owner() {
       <Heartbeat />
       <div className="flex-1 shadow-2xl shadow-gray-800">
         <div className="flex justify-between items-center p-4">
-          <Link to="/table ">
+          <Link to="/table">
             <FaShopLock className="h-10 w-10" />
           </Link>
           <h1 className="font-extrabold text-3xl text-center text-[#18196c] ml-6">Admin Panel</h1>
@@ -70,20 +71,39 @@ function Owner() {
           </button>
         </div>
 
-        <div className="m-2 mb-1 mt-2 ">
-          <div className="w-full h-auto p-6 md:h-56 rounded-3xl bg-[#18196c] flex flex-col items-center justify-center  transition duration-150 ease-in-out">
+        <div className="m-2 mb-1 mt-2">
+          <div className="w-full h-auto p-6 md:h-56 rounded-3xl bg-[#18196c] flex flex-col items-center justify-center transition duration-150 ease-in-out">
             <div className="flex flex-row items-center justify-between w-full px-8">
               <div className="text-white font-serif">
-                <h1 className="  text-2xl font-mono font-extrabold text-white mr-1 mb-4">{currentDate}</h1>
-                <h1 className="text-lg font-bold">Orders: <span className="font-bold">{memoizedStats.todaysOrders || 'N/A'}</span></h1>
-                <h1 className="text-lg font-bold">Profit: <span className="font-bold">{memoizedStats.todaysProfit || 'N/A'}</span></h1>
-                <h1 className="text-lg font-bold">Revenue: <span className="font-bold">{memoizedStats.todaysRevenue || 'N/A'}</span></h1>
-                <h1 className="text-lg font-bold">Units: <span className="font-bold">{memoizedStats.totalDishesSold || 'N/A'}</span></h1>
-             </div>
-             <div className="shadow-inner shadow-white ">
-                            <FaUserTie fill="white" className="h-16 w-16 m-4   " />
-                            </div>
-
+                <h1 className="text-2xl font-mono font-extrabold text-white mr-1 mb-4">{currentDate}</h1>
+                {showStats ? (
+                  <>
+                    <h1 className="text-lg font-bold">Orders: <span className="font-bold">{memoizedStats.todaysOrders || 'N/A'}</span></h1>
+                    <h1 className="text-lg font-bold">Profit: <span className="font-bold">{memoizedStats.todaysProfit || 'N/A'}</span></h1>
+                    <h1 className="text-lg font-bold">Revenue: <span className="font-bold">{memoizedStats.todaysRevenue || 'N/A'}</span></h1>
+                    <h1 className="text-lg font-bold">Units: <span className="font-bold">{memoizedStats.totalDishesSold || 'N/A'}</span></h1>
+                  </>
+                ) : (
+                  <>
+                    <h1 className="text-lg font-bold">Orders: N/A</h1>
+                    <h1 className="text-lg font-bold">Profit: N/A</h1>
+                    <h1 className="text-lg font-bold">Revenue: N/A</h1>
+                    <h1 className="text-lg font-bold">Units: N/A</h1>
+                  </>
+                )}
+              </div>
+              <div className="shadow-inner shadow-white">
+                <FaUserTie fill="white" className="h-16 w-16 m-4" />
+                <label className="flex items-center mt-2">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox h-5 w-5 text-blue-600"
+                    checked={showStats}
+                    onChange={() => setShowStats(!showStats)}
+                  />
+                  <span className="ml-2 text-white">Show Stats</span>
+                </label>
+              </div>
             </div>
           </div>
         </div>
@@ -92,13 +112,13 @@ function Owner() {
           <Link to="/Order">
             <div className="p-3 h-56 rounded-3xl transition duration-150 ease-in-out bg-[#18196c] flex flex-col items-center justify-center">
               <h1 className="text-lg font-mono font-extrabold text-white">Order Details</h1>
-              <GiCook fill="white" className="h-12 w-12 mt-4  shadow-2xl shadow-white" />
+              <GiCook fill="white" className="h-12 w-12 mt-4 shadow-2xl shadow-white" />
             </div>
           </Link>
           <Link to="/Takeaway">
             <div className="p-3 h-56 rounded-3xl transition duration-150 ease-in-out bg-[#18196c] flex flex-col items-center justify-center">
               <h1 className="text-lg font-mono font-extrabold text-white">Create Bill</h1>
-              <FaFileInvoiceDollar fill="white" className="h-12 w-12 mt-4  shadow-2xl shadow-white" />
+              <FaFileInvoiceDollar fill="white" className="h-12 w-12 mt-4 shadow-2xl shadow-white" />
             </div>
           </Link>
           <Link to="/Website">
@@ -110,7 +130,7 @@ function Owner() {
           <Link to="/Dishmanage">
             <div className="p-3 h-56 rounded-3xl transition duration-150 ease-in-out bg-[#18196c] flex flex-col items-center justify-center">
               <h1 className="text-lg font-mono font-extrabold text-white">Dish Management</h1>
-              <IoFastFoodSharp className="h-12 w-12 mt-4 shadow-2xl shadow-white" fill="white " />
+              <IoFastFoodSharp className="h-12 w-12 mt-4 shadow-2xl shadow-white" fill="white" />
             </div>
           </Link>
           <Link to="/Waiter">
@@ -131,31 +151,29 @@ function Owner() {
               <FaChartLine fill="white" className="h-12 w-12 mt-4 shadow-2xl shadow-white" />
             </div>
           </Link>
-          <Link to="/Month">
+          <Link to="/Order">
             <div className="p-3 h-56 rounded-3xl transition duration-150 ease-in-out bg-[#18196c] flex flex-col items-center justify-center">
-              <h1 className="text-lg font-mono font-extrabold text-white">Revenue-Profit</h1>
+              <h1 className="text-lg font-mono font-extrabold text-white">Total Report</h1>
               <TbReport fill="white" className="h-12 w-12 mt-4 shadow-2xl shadow-white" />
             </div>
           </Link>
         </div>
-        <footer className="bg-gray-700 text-white p-4 text-center">
-          <p>© 2024 Cafehouse Pvt ltd. All rights reserved.</p>
-        </footer>
       </div>
+
       {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+          <div className="bg-white p-6 rounded-lg">
             <h2 className="text-lg font-bold mb-4">Confirm Logout</h2>
-            <p className="mb-4">Are you sure you want to log out?</p>
-            <div className="flex justify-end space-x-4">
+            <p className="mb-4">Are you sure you want to logout?</p>
+            <div className="flex justify-end">
               <button
-                className="px-4 py-2 bg-gray-500 text-white rounded"
+                className="bg-gray-300 px-4 py-2 rounded mr-2"
                 onClick={() => setShowModal(false)}
               >
                 Cancel
               </button>
               <button
-                className="px-4 py-2 bg-red-500 text-white rounded"
+                className="bg-red-500 text-white px-4 py-2 rounded"
                 onClick={handleLogout}
               >
                 Logout
