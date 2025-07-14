fd4d59e812ecc8da086828b2d592d4b958bef7d8
Raunaksingh02 <158809081+Raunaksingh02@users.noreply.github.com>
2025-07-14 16:04:40 +0530

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -133,7 +133,7 @@ function Homepage() {
     if (loading) {
         return (
             <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-yellow-300 to-yellow-200">
-                <h1 className="font-extrabold text-2xl text-center mb-4">Loading the menu...</h1>
+                <h1 className="font-extrabold text-2xl text-center mb-4">Loading the products...</h1>
                 <GrCafeteria className="h-10 w-10" />
                 <img
                     src={deliverylogo}
